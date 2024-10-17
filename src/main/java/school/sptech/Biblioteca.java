0d3ff6180ce6f5d4856adb8f44e4de6e59549235
bruno-yuji <bruno.ytakahashi@sptech.school>
2024-10-17 15:27:09 -0300

    Bruno Yuji Takahashi - RA: 04241032

--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,114 @@
 package school.sptech;
 
-public class Biblioteca {
+import school.sptech.exception.ArgumentoInvalidoException;
+import school.sptech.exception.LivroNaoEncontradoException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+    private String nome;
+    private List<Livro> livros;
+
+    public Biblioteca(String nome) {
+        this.nome = nome;
+        this.livros = new ArrayList<>();
+    }
+
+    public Biblioteca() {
+        this.livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        if (livro != null && livro.getAutor() != null && !livro.getAutor().isBlank() &&
+                !livro.getAutor().isEmpty() && livro.getTitulo() != null &&
+                !livro.getTitulo().isBlank() && !livro.getTitulo().isEmpty() &&
+                 livro.getDataPublicacao() != null) {
+            livros.add(livro);
+        } else {
+            throw new ArgumentoInvalidoException("Os valores passados são inválidos!");
+        }
+    }
+
+    public void removerLivroPorTitulo(String titulo) {
+        if (titulo != null && !titulo.isBlank() && !titulo.isEmpty()) {
+            Boolean encontrado = false;
+            for (int i = 0; i < livros.size(); i++) {
+                if (livros.get(i).getTitulo().equalsIgnoreCase(titulo)) {
+                    livros.remove(i);
+                    encontrado = true;
+                }
+            }
+            if (!encontrado) {
+                throw new LivroNaoEncontradoException("O livro não foi encontrado.");
+            }
+        } else {
+            throw new ArgumentoInvalidoException("Título inválido!");
+        }
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo) {
+        if (titulo != null && !titulo.isBlank() && !titulo.isEmpty()) {
+            for (Livro livro : livros) {
+                if (livro.getTitulo().equalsIgnoreCase(titulo)) {
+                    return livro;
+                }
+            }
+            throw new LivroNaoEncontradoException("O livro não foi encontrado.");
+        }
+        throw new ArgumentoInvalidoException("Título inválido!");
+    }
+
+    public Integer contarLivros() {
+        return livros.size();
+    }
+
+    public List<Livro> obterLivrosAteAno(Integer ano) {
+        List<Livro> livrosPublicados = new ArrayList<>();
+        for (Livro livro : livros) {
+            if (livro.getDataPublicacao().getYear() <= ano) {
+                livrosPublicados.add(livro);
+            }
+        }
+        return livrosPublicados;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Livro> retornarTopCincoLivros() {
+        List<Livro> top5 = new ArrayList<>();
+        if (livros.size() > 0) {
+            if (livros.size() == 1) {
+                top5.add(livros.getFirst());
+            } else {
+                List<Livro> livrosCopia = this.livros;
+                Livro livroTemporario;
+                for (int cont = 0; cont < livros.size()-1; cont++) {
+                    for (int i = 0; i < livros.size()-1; i++) {
+                        if (livrosCopia.get(i).calcularMediaAvaliacoes() < livrosCopia.get(i+1).calcularMediaAvaliacoes()) {
+                            livroTemporario = livrosCopia.get(i);
+                            livrosCopia.set(i, livrosCopia.get(i+1));
+                            livrosCopia.set(i+1, livroTemporario);
+                        }
+                    }
+                }
+                Integer tamanho;
+                if (livros.size() < 5) {
+                    tamanho = livros.size();
+                } else {
+                    tamanho = 5;
+                }
+                for (int i = 0; i < tamanho; i++) {
+                    top5.add(livrosCopia.get(i));
+                }
+            }
+        }
+        return top5;
+    }
 }
--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -1,8 +1,23 @@
 package school.sptech;
 
+
+import java.time.LocalDate;
+
 public class Main {
-
     public static void main(String[] args) {
+        LocalDate data = LocalDate.now();
 
+        Livro livro1 = new Livro("Teste", "David", data);
+        livro1.adicionarAvaliacao("bom", 5.0);
+        Livro livro2 = new Livro("Teste", "Hal", data);
+        livro2.adicionarAvaliacao("mediano", 3.0);
+        Livro livro3 = new Livro("Teste", "Oliver", data);
+        livro3.adicionarAvaliacao("bom", 4.0);
+
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.adicionarLivro(livro1);
+        biblioteca.adicionarLivro(livro2);
+        biblioteca.adicionarLivro(livro3);
+        System.out.println(biblioteca.retornarTopCincoLivros());
     }
 }
